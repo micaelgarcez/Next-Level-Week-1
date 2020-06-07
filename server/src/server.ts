32fe4dbc3376dcc01ef0939d831899acd41c77a4
micaelgarcez <micaelgarcez@outlook.com>
2020-06-06 22:15:05 -0300

    Backend da Aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('listagem');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    // JSON
-
-    res.json([
-        'Mike',
-        'micael'
-    ])
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
